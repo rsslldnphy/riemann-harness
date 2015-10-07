15e5734e544ff28d28bc17a09edef9de3734417b
Russell <russell@russelldunphy.com>
2015-10-07 18:42:27 +0100

    First commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to riemann-harness
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject riemann-harness "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :main ^:skip-aot riemann-harness.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
